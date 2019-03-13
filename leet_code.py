709614f46ae09cef7819450bdf73095847816d80
gtg162y <amitpatel.gt@gmail.com>
2019-03-13 16:46:18 -0700

    Update leet_code.py

--- leet_code.py
+++ leet_code.py
@@ -1,1 +1,30 @@
 #test
+
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+     
+    def setData(self,data):
+        self.data = data
+      
+    def getData(self):
+        return self.data
+     
+    def setNext(self,next):
+        self.next = next
+     
+    def getNext(self):
+        return self.next
+        
+class SinglyLinkedList:
+    #constructor
+    def __init__(self):
+        self.head = None
+        
+    #method for setting the head of the Linked List
+    def setHead(self,head):
+        self.head = head
+                     
+    def arrange_in_pairs(self):
+        
