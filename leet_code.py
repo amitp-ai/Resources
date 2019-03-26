76607ef9aa14e7fa76b0399c72fc9d7c3d9980cc
gtg162y <amitpatel.gt@gmail.com>
2019-03-25 21:12:47 -0700

    Update leet_code.py

--- leet_code.py
+++ leet_code.py
@@ -2,19 +2,3 @@
 
 
 
-def Pascal_Triange(n):
-    res = []
-    if n <= 0: return res
-    res.append([1])
-    if n == 1: return res
-    res.append([1,1])
-    if n == 2: return res
-    for i in range(2,n):
-        temp = []
-        temp.append(1)
-        for j in range(len(res[i-1])-1):
-            temp.append(res[i-1][j] + res[i-1][j+1])
-        temp.append(1)
-        res.append(temp)
-    print(res)
-Pascal_Triange(5)
