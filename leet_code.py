15f037af5d75660962290dd62940266b3170f164
gtg162y <amitpatel.gt@gmail.com>
2020-02-04 11:44:24 -0800

    Update leet_code.py
    Example using Python decorators with parameter

--- leet_code.py
+++ leet_code.py
@@ -1,1 +1,34 @@
 #test
+
+# Decorators with parameters in Python
+def big_func(dumy):
+    print(dumy)
+    return func1
+
+def func1(x):
+    print('xxxxx')
+    print(x)
+
+big_func('zzzz')(2)
+
+@big_func('zzz')
+def func1(x):
+    print(x)
+
+print('1'*50)
+
+def decorator(*args, **kwargs): 
+    print("Inside decorator") 
+    def inner(func): 
+        print("Inside inner function") 
+        print("I like", kwargs['like'])  
+        return func 
+    return inner 
+@decorator(like = "geeksforgeeks") 
+def func(): 
+    print("Inside actual function") 
+func() 
+
+print('2'*50)
+
+decorator(like = "geeksforgeeks")(func)()
