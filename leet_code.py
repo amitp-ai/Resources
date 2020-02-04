4b43032bb69b2bdc99c2624728d3b3993410bf34
gtg162y <amitpatel.gt@gmail.com>
2020-02-04 12:01:09 -0800

    Update leet_code.py

--- leet_code.py
+++ leet_code.py
@@ -1,5 +1,29 @@
 #test
 
+"""
+Syntax for decorators with parameters
+
+def decorator(p):
+    def inner_func():
+        #do something
+     return inner_func
+
+@decorator(params)
+def func_name():
+    ''' Function implementation'''
+The above code is equivalent to
+
+def func_name():
+    ''' Function implementation'''
+
+func_name = (decorator(params))(func_name) #same as decorator(params)(func_name)
+
+As the execution starts from left to right decorator(params) is called which returns a function object fun_obj. 
+Using the fun_obj the call fun_obj(fun_name) is made. Inside the inner function, required operations are performed and 
+the actual function reference is returned which will be assigned to func_name. Now, func_name() can be used to call the function 
+with decorator applied on it.
+"""
+
 # Decorators with parameters in Python
 def big_func(dumy):
     print(dumy)
